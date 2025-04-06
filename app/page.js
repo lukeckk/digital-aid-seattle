5441c5d3dceafcd7bdfd903fa6d73bd0261b98f7
Luke Cheng <Lukeecheng@gmail.com>
2025-04-06 14:58:23 -0700

    set dashboard as root

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
-import { redirect } from 'next/navigation';
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
-  redirect('/dashboard');
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
 }
